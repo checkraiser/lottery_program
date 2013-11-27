634e34a22cabf1905ffab7587ea28fe5a13e9c03
hoasung01 <nguyenngochai.shipagent@gmail.com>
2013-11-27 11:03:30 +0700

    Update lottery_spec.rb

--- spec/lottery_spec.rb
+++ spec/lottery_spec.rb
@@ -6,10 +6,13 @@ describe "Lottery" do
 		l.serial_number.should == "12"
 	end
 	
-	it "has an import CSV file" do
-		require 'CSV gems'
-		CSV.read_by_line('filename')
-		puts " #{count} tickets were sold "
-		
+	it "read a file text" do
+		l = Lottery.new
+		l = File.open('filetoread').to_read_by_line
+	        l[0] = 12345, 12
+	        l[1] = 99999, 20
+		l.length.should ==" #{l.length}ticket were sold!"
 	end
+	
+end  
 end
